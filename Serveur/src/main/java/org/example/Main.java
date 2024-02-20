96440edd8e84e4fadbc4845ab3266487a69f2950
Guillaume Paoli <wdgaster.gtp@gmail.com>
2024-02-20 09:42:09 -0500

    Connexion cote serveur

--- Serveur/src/main/java/org/example/Main.java
+++ Serveur/src/main/java/org/example/Main.java
@@ -1,17 +1,60 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ServerSocket serverSocket;
+        Socket clientSocket;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        try {
+            //lire le port
+            serverSocket = new ServerSocket(8080);
+
+            //connexion
+            clientSocket = serverSocket.accept();
+            System.out.printf("DEBUG: Connexion d'un client: %s\n", clientSocket.getInetAddress().getHostAddress());
+
+            //Flux entre et sorti
+            PrintWriter socketOut = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader socketIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            //message de bienvenue
+            socketOut.printf("Bienvenue sur le pendu des noms des personnage de Genshin Impact.\n");
+
+            //commencer une parti?
+            while (true) {
+                socketOut.printf("Voulez vous jouez une parti? [Y or N]");
+                String choice = socketIn.readLine();
+                System.out.println("DEBUG: Choix de client: " + choice);
+
+                //commencer une partir
+                if (choice.matches("Y|y|yes|Yes|Oui|oui")) {
+                    socketOut.printf("Debut de la partie");
+                    socketOut.printf("Fin de la partie");
+                }
+                //fermer le client et le serveur
+                else if (choice.matches("N|n|No|no|Non|non")) {
+                    break;
+                }
+                //recommencer la boucle
+                else {
+                    socketOut.printf("Reponse non valide.");
+                }
+            }
+
+            //fermer le serveur
+            clientSocket.close();
+            serverSocket.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
